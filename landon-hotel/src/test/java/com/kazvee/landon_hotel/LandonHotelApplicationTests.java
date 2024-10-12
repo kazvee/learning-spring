6478fcf3e9d8b424cafe4797b8522c7cd26720a2
kazvee <kazveedev@gmail.com>
2024-10-12 15:14:46 -0600

    Initial commit

---
+++ landon-hotel/src/test/java/com/kazvee/landon_hotel/LandonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazvee.landon_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
