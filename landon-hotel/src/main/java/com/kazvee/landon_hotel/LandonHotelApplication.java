fe6de4ebe12a2d107f46ebf49ab5ee414590b011
kazvee <kazveedev@gmail.com>
2024-10-12 18:32:18 -0600

    Load app properties from dotenv file

--- landon-hotel/src/main/java/com/kazvee/landon_hotel/LandonHotelApplication.java
+++ landon-hotel/src/main/java/com/kazvee/landon_hotel/LandonHotelApplication.java
@@ -1,5 +1,6 @@
 package com.kazvee.landon_hotel;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LandonHotelApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.application.name", dotenv.get("SPRING_APPLICATION_NAME"));
+		System.setProperty("spring.jpa.database", dotenv.get("SPRING_JPA_DATABASE"));
+		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(LandonHotelApplication.class, args);
 	}
-
 }
