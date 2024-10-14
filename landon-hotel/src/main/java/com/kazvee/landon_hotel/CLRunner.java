77b75613b9f3ac9e6ce7adce43a022f5ce32b772
kazvee <kazveedev@gmail.com>
2024-10-14 11:58:06 -0600

    Update CLRunner to print all data

--- landon-hotel/src/main/java/com/kazvee/landon_hotel/CLRunner.java
+++ landon-hotel/src/main/java/com/kazvee/landon_hotel/CLRunner.java
@@ -4,24 +4,37 @@ import org.springframework.stereotype.Component;
 
 import com.kazvee.landon_hotel.data.entity.Room;
 import com.kazvee.landon_hotel.data.repository.RoomRepository;
+import com.kazvee.landon_hotel.data.entity.Guest;
+import com.kazvee.landon_hotel.data.repository.GuestRepository;
+import com.kazvee.landon_hotel.data.entity.Reservation;
+import com.kazvee.landon_hotel.data.repository.ReservationRepository;
 import org.springframework.boot.CommandLineRunner;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class CLRunner implements CommandLineRunner {
 
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public CLRunner(RoomRepository roomRepository) {
+    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository,
+            ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        List<Room> rooms = roomRepository.findAll();
-        Optional<Room> room = roomRepository.findByRoomNumberIgnoreCase("p1");
-        System.out.println(room);
+        System.out.println("*** GUESTS ***");
+        List<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        System.out.println("*** ROOMS ***");
+        List<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+        System.out.println("*** RESERVATIONS ***");
+        List<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 }
